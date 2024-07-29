3b401e618c8160dbeb2c71d0d2670374b2491f99
Angel De La Cruz <122130728+taos15@users.noreply.github.com>
2024-07-29 17:07:11 -0400

    adds console.log for the page context.

--- src/setupPnP.ts
+++ src/setupPnP.ts
@@ -14,6 +14,7 @@ let sp: ReturnType<typeof spfi>;
 
 export function setupPnP() {
     if (window._spPageContextInfo) {
+        console.log(window._spPageContextInfo);
         sp = spfi().using(SPFx({ pageContext: window._spPageContextInfo }));
         console.log("PnPjs initialized with SharePoint context.");
     } else {
